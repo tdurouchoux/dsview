dfd41b0008f47bb4e2618781f23a9258374585df
thibaultdu <39222414+thibaultdu@users.noreply.github.com>
2024-12-15 01:39:23 +0100

    fix docker setup

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11-slim-bookworm
+FROM python:3.11-slim
 COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
 
 RUN apt-get update && \
--- docker_startup.sh
+++ docker_startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /app/.venv/bin/python dsview/obsidian/setup.py
 
-/app/.venv/bin/fastapi run dsview/api.py & \
-/app/.venv/bin/streamlit run dsview/labelling/interface.py
+/app/.venv/bin/fastapi run dsview/api.py --host 0.0.0.0 & \
+/app/.venv/bin/streamlit run dsview/labelling/interface.py --server.address 0.0.0.0
