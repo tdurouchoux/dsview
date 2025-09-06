5d8213f9242526e42872c0a9e52a8cc23dc20ce6
tdurouchoux <thibault.durouchoux@gmail.com>
2025-09-06 22:47:46 +0000

    fix clone vault

--- dsview/obsidian/setup.py
+++ dsview/obsidian/setup.py
@@ -14,7 +14,7 @@ def init_vault():
     logger.info("Initializing vault...")
     if obsidian_config.github_vault.repository is not None:
         logger.info("Github vault enabled, cloning repository ...")
-        run(["git", "clone", "--depth 1", obsidian_config.github_vault.repository])
+        run(["git", "clone", obsidian_config.github_vault.repository])
 
         directory_name = obsidian_config.github_vault.repository.split("/")[-1].replace(
             ".git", ""
