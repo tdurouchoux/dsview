adaa99d12be870f773aa5b4ae135a4abe62c6051
tdurouchoux <thibault.durouchoux@gmail.com>
2025-09-14 12:28:07 +0000

    Adding more debug info for vault cloning

--- dsview/obsidian/setup.py
+++ dsview/obsidian/setup.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from subprocess import run
+import subprocess
 
 from dsview import config
 
@@ -9,29 +9,56 @@ logger = logging.getLogger("setup_vault")
 
 obsidian_config = config.load_obsidian_config()
 
+class CommandFailed(Exception):
+
+    def __init__(self, cmd_label: str, stdout: str, stderr: str):
+        super().__init__(
+            f"Command {cmd_label} failed with stdout : '{stdout}'"
+            f" and stderr '{stderr}'"
+        )
+
+
+def run_cmd(cmd: list[str], cmd_label: str, ignore_error: bool = False, cwd: Path | None = None):
+    try:
+        result = subprocess.run(
+            cmd,
+            capture_output=True,  # Captures both stdout and stderr
+            text=True,            # Decodes to str instead of bytes
+            check=True,            # Raises CalledProcessError if non-zero exit code
+            cwd=cwd,
+        )
+        logger.info("Command '%s' ran successfully", cmd_label)
+        logger.debug("with stdout : %s", result.stdout)
+
+    except subprocess.CalledProcessError as e:
+
+        logger.error("Failed to run command '%s' with stderr : %s", cmd_label, e.stderr)
+
+        if not ignore_error:
+            raise CommandFailed(
+                cmd_label,
+                e.stdout,
+                e.stderr
+            )
+
 
 def init_vault():
     logger.info("Initializing vault...")
     if obsidian_config.github_vault.repository is not None:
         logger.info("Github vault enabled, cloning repository ...")
-        logger.info("Url used : %s", obsidian_config.github_vault.url)
 
-        run(["git", "clone", obsidian_config.github_vault.url])
+        run_cmd(["git", "clone", obsidian_config.github_vault.url, obsidian_config.vault_path], "clone vault")
 
-        directory_name = obsidian_config.github_vault.repository.split("/")[-1].replace(
-            ".git", ""
-        )
-
-        run(
+        run_cmd(
             ["git", "config", "user.name", obsidian_config.github_vault.username],
-            cwd=Path(directory_name).resolve(),
+            "git username configuration",
+            cwd=Path(obsidian_config.vault_path).resolve(),
         )
-        run(
+        run_cmd(
             ["git", "config", "user.email", obsidian_config.github_vault.email],
-            cwd=Path(directory_name).resolve(),
+            "git user email configuration",
+            cwd=Path(obsidian_config.vault_path).resolve(),
         )
-
-        Path(directory_name).rename(obsidian_config.vault_path)
 
         return
 
